a9c028c226d5a7b70ba55876683e23f27f230ecf
Deepthi S <130971935+DeepthiShiv@users.noreply.github.com>
2025-01-22 22:53:27 +0530

    Create selectionSort.java

---
+++ selectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class sorting{
+
+    //selection sort
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minPos = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPos]>arr[j]){
+                    minPos=j;
+                }
+            }
+            int temp=arr[minPos];
+            arr[minPos]= arr[i];
+            arr[i]=temp;
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[]={4,5,3,1,2};
+        selectionSort(arr);
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
